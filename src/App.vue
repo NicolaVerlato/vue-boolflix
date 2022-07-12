2549776cf2f63c93de78f62980bd0b095524b92b
NicolaVerlato <nicola.verlato00@gmail.com>
2022-07-12 18:03:39 +0200

    mancano bandiere milestone 2

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
     </header>
 
     <main>
-      <MainContent v-for="movie, index in filteredMovie" :key="index" :movieInfo="movie"/>
+      <MainContent v-for="movie, index in movies" :key="index" :movieInfo="movie"/>
+      <SeriesComponent v-for="series, index in series" :key="index" :seriesInfo="series"/>
     </main>
   </div>
 </template>
@@ -13,39 +14,55 @@
 <script>
 import HeaderComponent from './components/HeaderComponent.vue'
 import MainContent from './components/MainContent.vue'
+import SeriesComponent from './components/SeriesComponent.vue'
 import axios from "axios"
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
-    MainContent
+    MainContent,
+    SeriesComponent
   },
   data(){
       return{
           myApi: 'efb8e071130f0936789711365b3fd8b4',
           movies: [],
+          series: [],
           search: ''
       }
   },
   computed:{
     filteredMovie(){
       return this.movies.filter((item) => {
-        return item.title.toLowerCase().includes(this.search.toLowerCase());  
+        return  item.title.toLowerCase().includes(this.search.toLowerCase());  
+      })
+    },
+    filteredSeries(){
+      return this.series.filter((item) => {
+        return  item.name.toLowerCase().includes(this.search.toLowerCase());  
       })
     }
   },
   methods:{
       getMovie(){
-          axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.myApi}&query=${this.search}`)
-          .then((element) => {
-            this.movies = element.data.results
-            console.log(this.movies)
-          })
+        // chiamata per film
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.myApi}&query=${this.search}`)
+        .then((element) => {
+          this.movies = element.data.results;
+        })
+      },
+      getSeries(){
+        // chiamata per serie tv
+        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${this.myApi}&language=it_IT&query=${this.search}`)
+        .then((element) => {
+          this.series = element.data.results;
+        })
       },
       startSearch(text){
-      this.search = text
-      this.getMovie()
+      this.search = text;
+      this.getMovie();
+      this.getSeries();
     }
   },
 }
---
+++ src/components/SeriesComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card-container">
+        <h2>{{ seriesInfo.name }}</h2>
+        <div>Titolo Originale: {{ seriesInfo.original_name }}</div>
+        <div>Lingua: {{ seriesInfo.original_language }}</div>
+        <div>Voto: {{ seriesInfo.vote_average }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SeriesComponent',
+    props:{
+        seriesInfo: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-container{
+    padding: 20px;
+    width: calc(20% - 20px);
+    text-align: center;
+    margin: 10px;
+}
+</style>
