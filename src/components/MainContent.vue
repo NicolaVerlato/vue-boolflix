ac6805808abd5149f48d28654091d67289eb6f8c
NicolaVerlato <nicola.verlato00@gmail.com>
2022-07-13 11:02:01 +0200

    iniziata milestone 3

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="card-container">
+        <img v-if="movieInfo.poster_path" :src="`http://image.tmdb.org/t/p/w300/${movieInfo.poster_path}`" :alt="movieInfo.name">
+        <img v-else src="https://placehold.jp/30/000000/ffffff/300x450.png?text=Copertina+non+disponibile" alt="copertina">
         <h2>{{ movieInfo.title }}</h2>
         <div>Titolo Originale: {{ movieInfo.original_title }}</div>
         <div>Lingua: {{ movieInfo.original_language }}</div>
@@ -22,5 +24,6 @@ export default {
     text-align: center;
     margin: 10px;
     background-color: lightcoral;
+
 }
 </style>
--- src/components/SeriesComponent.vue
+++ src/components/SeriesComponent.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="card-container">
+        <img v-if="seriesInfo.poster_path" :src="`http://image.tmdb.org/t/p/w342/${seriesInfo.poster_path}`" :alt="seriesInfo.name">
+        <img v-else src="https://placehold.jp/30/000000/ffffff/300x450.png?text=Copertina+non+disponibile" alt="copertina">
         <h2>{{ seriesInfo.name }}</h2>
         <div>Titolo Originale: {{ seriesInfo.original_name }}</div>
         <div>Lingua: {{ seriesInfo.original_language }}</div>
